e70e341b66a5f9707164daa634ab27d576b1a2a4
lysenkoj@gmail.com <Jordan@Jordans-MacBook-Air.local>
2018-09-11 20:39:46 -0400

    stripping out old code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import {Switch, Route} from 'react-router';
-import ArrayPage from './components/arrayPage';
 import Header from './components/header.jsx';
 import Home from './components/home.jsx';
 import Footer from './components/footer';
@@ -14,7 +13,6 @@ class App extends Component {
         <Header />
         <Switch>
           <Route exact path="/" component={Home} />
-          <Route path='/:gender' component={ArrayPage} />
         </Switch>
         <Footer />
       </div>
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-import logo from '../images/jcrewlogo.png';
-// import '../Sass/Content.sass';
 
 
 export default class Header extends Component {
@@ -10,9 +8,6 @@ export default class Header extends Component {
   render(){
     return(
       <div id="Header-Container">
-        <Link id='Home-Link' to='/' >
-          <img src={logo} />
-        </Link>
       </div>
     )
   }
