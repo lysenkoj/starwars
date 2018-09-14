b787df8ab03447f8dba20c9530425631f012647c
lysenkoj@gmail.com <Jordan@Jordans-MacBook-Air.local>
2018-09-13 21:39:04 -0400

    loading animation working

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -14,6 +14,16 @@ export default class HOME extends Component {
       data: null
     };
 
+    // Component Hasn't Mounted Yet
+    const home = document.querySelector('div#Home-Container');
+    const loadingText = document.querySelector('h2#Loading-Text');
+    const loader = document.querySelector('div#Loader');
+
+    // home.style.display = 'none';
+    loader.style.display = 'flex';
+    this.animateText(loadingText);
+
+
   }
 
   setCharacterData(){
@@ -21,6 +31,97 @@ export default class HOME extends Component {
       previousState.characters = characters.characters;
       return previousState;
     })
+  }
+
+  animateText(text){
+    let loading = [['L','L'], ['O','O'],['A','A'],['D','D'],['I','I'], ['N','N'],['G','G'],['Dot1', '.'], ['Dot2', '.'], ['Dot3', '.']];
+    let count = 0;
+    let min = 0;
+
+    for(let i = 0; i < loading.length; i++){
+      text.innerHTML += `<span id=${loading[i][0]} class='Loading-Text'>${loading[i][1]}</span>`;
+    }
+
+
+    var interval = setInterval(()=>{
+      const random = Math.floor(Math.random() * (10 - min) + min);
+      const fullWord = document.querySelectorAll('span.Loading-Text');
+      const wordSelectors = ['L','O','A','D','I','N','G','Dot1','Dot2','Dot3'];
+      const letter = document.querySelector(`span#${wordSelectors[random]}`);
+      const randomCharCode = Math.floor(Math.random() * (92 - 33) + 33);
+
+      const setCorrect = () => {
+        let word = 'LOADING...'
+        const span = document.querySelector(`span#${wordSelectors[min]}`);
+        span.innerHTML = word[min];
+        span.style['font-family'] = 'Metropolis-Regular';
+      }
+
+      if(count%10 && count >= 100){
+        fullWord.forEach(letter => {
+          letter.style.opacity = 1;
+        })
+      }else if(count >= 100){
+        fullWord.forEach(letter => {
+          letter.style.opacity = 0;
+        })
+      }
+
+      if(count < 100){
+        letter.innerHTML = String.fromCharCode(randomCharCode);
+      }
+
+      console.log(count, min);
+
+      count++;
+
+      switch(count){
+        case 300:
+          clearInterval(interval);
+          break;
+        case 100:
+          setCorrect();
+          break;
+        case 90:
+          setCorrect();
+          min++;
+          break;
+        case 80:
+          setCorrect();
+          min++;
+          break;
+        case 70:
+          setCorrect();
+          min++;
+          break;
+        case 60:
+          setCorrect();
+          min++;
+          break;
+        case 50:
+          setCorrect();
+          min++;
+          break;
+        case 40:
+          setCorrect();
+          min++;
+          break;
+        case 30:
+          setCorrect();
+          min++;
+          break;
+        case 20:
+          setCorrect();
+          min++;
+          break;
+        case 10:
+          setCorrect();
+          min++;
+          break;
+      }
+
+    }, 60);
+
   }
 
   render(){
